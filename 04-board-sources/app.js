282b1b24233a02fe026364cda3057a17db793a2e
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2022-06-29 23:42:11 +0300

    Add board files

---
+++ 04-board-sources/app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#518DDB', '#2AF4DA', '#4CE3B3', '#E03EF3', '#5DDB45', '#F5EE3D', '#F5A72C', '#661CBA']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10 px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
